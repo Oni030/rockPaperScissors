95b9a2b68c4ec5a2dfabf8a4186b4224402181d4
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-05-20 17:57:19 +0200

    Declaring variables for html elements. Elements will be used to retrieve user-input and to display of processed data.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Retrieve all elements from html
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const actionDisplay = document.querySelector(".action-display");
+const playerScore = docuemnt.querySelector(".player-score");
+const computerScore = docuemnt.querySelector(".computer-score");
+const resetButton = document.getElementById("reset-button");
+
 function getComputerChoice() {
     let weapons = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * 3);
